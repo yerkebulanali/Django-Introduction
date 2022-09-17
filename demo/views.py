aa6d9569b5ee132b788c80b0a468ea6b9c664c15
Yerkebulan <yerkebulanali@gmail.com>
2022-09-17 22:10:54 +0600

    dynamic templates

--- demo/views.py
+++ demo/views.py
@@ -5,4 +5,7 @@ from .models import Book
 from django.shortcuts import render
 
 def first(request):
-    return render(request, 'first_temp.html')
+
+    books = Book.objects.all()
+
+    return render(request, 'first_temp.html', {'books': books})
